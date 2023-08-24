824366fbb36e94352df70064ebc58903e205bfc8
Przemek Konieczny <przemyslawkoniecznyy@gmail.com>
2023-08-24 16:45:07 +0200

    new commit

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import "tw-elements/dist/css/tw-elements.min.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 
+
 config.autoAddCss = false;
 
 const inter = Inter({ subsets: ['latin'] })
