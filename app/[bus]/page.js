13e316765013da476ddc62d75a2836ea8c0b452a
Przemek Konieczny <przemyslawkoniecznyy@gmail.com>
2023-08-24 19:17:03 +0200

    new commit

--- app/[bus]/page.js
+++ app/[bus]/page.js
@@ -3,7 +3,7 @@ import RozkładClient from "../(components)/RozkładClient";
 export default async function Bus({ params, searchParams }) {
   const hours = searchParams.hours;
 
-  const data = await fetch(`http://localhost:3000/api/${params.bus}`);
+  const data = await fetch(`https://buss-application.vercel.app/api/${params.bus}`);
 
   const res = await data.json();
   return(<RozkładClient res={res} hours={hours} bus={params.bus} />)
